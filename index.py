168a4965b984a1186fdb10eb3382829137455908
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2019-06-28 23:30:30 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
+import pyfirmata
+import time
 from firebase import firebase
-firebase = firebase.FirebaseApplication("https://skripsiph.firebaseio.com/")
-data = {
-	'nama': 'Lilis',
-	'test': 'test'
-}
 
-result = firebase.post('/python-sample', data)
-print(result)
+board = pyfirmata.Arduino("/dev/ttyACM0")
+pin0  = board.get_pin('a:0:i')
+
+iterator = pyfirmata.util.Iterator(board)
+iterator.start()
+pin0.enable_reporting()
+
+starttime = time.time()
+
+while True:
+    if pin0.read() == None:
+        pass
+    else:
+        data = (1000 * pin0.read())/73.07
+        print("pH air: "+str(data))
+
+time.sleep(10.0 - ((time.time() - starttime) % 10.0))
